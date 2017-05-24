7b5bc26adfe1c5a2a579c565acfce11f1f8a2b36
Daan Rosbergen <d.rosbergen@gmail.com>
2017-05-24 11:35:45 +0200

    Set width to 940px. Add header image

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,6 +8,8 @@ import {
     Popover,
     Position
 } from "@blueprintjs/core";
+import './Nav.css';
+import img from './hsleiden.jpg';
 
 const menu = (
     <Menu>
@@ -19,22 +21,45 @@ const menu = (
     </Menu>
 );
 
+const wrapper = {position: 'relative'};
+const colorFilter = {
+    position: 'absolute',
+    backgroundColor: '#1539CF',
+    backgroundBlendMode: 'color',
+    zIndex: 10,
+    width: '100%',
+    height: '100%'
+};
+const image = {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    backgroundImage: `url(${img})`,
+    backgroundBlendMode: 'darken',
+    minHeight: '300px'
+};
+
 class Nav extends Component {
     state = {
-        name: "🍪"
+        name: "Stuxnet."
     }
 
     render() {
         return (
             <div>
+                <div className="header">
+                    <div style={wrapper}>
+                        {/*<div style={colorFilter}></div>*/}
+                        <div style={image}></div>
+                    </div>
+                </div>
+
                 <nav className="pt-navbar pt-dark">
                     <div className="Container">
                         <div className="pt-navbar-group pt-align-left">
                             <div className="pt-navbar-heading">{this.state.name}</div>
-                            <Link className="pt-button pt-minimal" to="/">Home</Link>
-                            <Link className="pt-button pt-minimal" to="/buildings">Buildings</Link>
                         </div>
                         <div className="pt-navbar-group pt-align-right">
+                            <Link className="pt-button pt-minimal" to="/">Home</Link>
+                            <Link className="pt-button pt-minimal" to="/buildings">Buildings</Link>
                             <Popover content={menu} position={Position.BOTTOM_RIGHT}>
                                 <Button className="pt-minimal" iconName="cog" />
                             </Popover>
