e85fcaf951412efdcd3b983b3cb0d7f25425adef
Daan Rosbergen <d.rosbergen@gmail.com>
2017-05-24 16:35:53 +0200

    Fix test by wrapping them in MemoryRoutetr

--- src/containers/Home.test.js
+++ src/containers/Home.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './Home';
+import {MemoryRouter} from 'react-router-dom'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Home />, div);
+  ReactDOM.render(
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>, div);
 });
