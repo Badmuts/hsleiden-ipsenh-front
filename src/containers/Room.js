2d30c8babf5c2b5d21469cdef294685dd0687e13
Brandon0172 <brandonvanwijk@gmail.com>
2017-05-30 13:27:25 +0200

    add Barchart and fill with data of today

--- src/containers/Room.js
+++ src/containers/Room.js
@@ -1,10 +1,31 @@
 import React, {Component} from 'react';
 import {Spinner} from '@blueprintjs/core';
 import Moment from 'moment';
+var BarChart = require("react-chartjs").Bar;
 
+var chartData = {
+    labels: [],
+    datasets: [
+        {
+            fillColor: "#79D1CF",
+            strokeColor: "#79D1CF",
+            data: []
+        }
+    ]
+};
+
+var chartOptions = {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+    }
+};
 class Room extends Component {
     state = {room: null}
-
+    
     componentDidMount() {
         console.log();
         fetch(`http://localhost:3000/buildings/${this.props.match.params.buildingId}/rooms/${this.props.match.params.roomId}`)
@@ -22,10 +43,21 @@ class Room extends Component {
         
         this.logs = function() {
             return room.logs.map(log => {
+
+                var today = Moment().format('D');
+                if(Moment(log.time).format('D') == today) {
+                    chartData.labels.push(Moment(log.time).format('H'));
+                    chartData.datasets[0].data.push(log.occupation)
+                }
+                
                 return(
                 <li>Occupation: {log.occupation} Time: {Moment(log.time).format('LLLL')}</li>
                 );
             });
+        }
+
+        this.chart = function() {
+            return <BarChart data={chartData} options={chartOptions}/>
         }
         return(
             <div className="Container p-30">
@@ -34,12 +66,14 @@ class Room extends Component {
                 <div className="pt-card pt-elevation-0 pt-interactive">
                     <h5>{room.name}</h5>
                     <ul>{this.logs()}</ul>
+                    {this.chart()}
                 </div>
             
              ) : (
                     <Spinner />
                 )}
             </div>
+            
         )
     }
 }
