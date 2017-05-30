bec61fe4fb3f148fd5ae3a95fa35cc3d6a77a8c8
Daan Rosbergen <d.rosbergen@gmail.com>
2017-05-30 17:36:33 +0200

    Update style

--- src/containers/Hubs.js
+++ src/containers/Hubs.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import {Spinner, NonIdealState, Colors} from '@blueprintjs/core';
+import {Spinner, NonIdealState, Colors, Tag, Intent, Classes, Tooltip} from '@blueprintjs/core';
 
 import Header from './../components/Header';
 import Nav from './../components/Nav';
@@ -46,11 +46,35 @@ class Hubs extends Component {
                 <div style={style} className={"row " + (!hubs.length ? "center-xs" : "")}>
                     {hubs && !err ? (
                         hubs.map(hub => (
-                            <div className="col-xs-6">
-                                <div className="pt-card pt-elevation-0 pt-interactive box" key={hub.id}>
-                                    <h5> <span className="pt-icon-large pt-icon-pulse" style={{color: (hub.status) === 'online' ? Colors.GREEN2 : (hub.status === 'offline') ? Colors.RED2 : Colors.ORANGE2}}></span> {hub.name}</h5>
-                                    <p>{hub.serialNumber}</p>
-                                    <p>Sensors: {hub.sensors.length}</p>
+                            <div className="col-xs-6" key={hub.id}>
+                                <div className="pt-card pt-elevation-0 pt-interactive box" style={{marginBottom: '15px'}}>
+                                    <h5>
+                                        <span className="pt-icon-large pt-icon-pulse" style={{color: (hub.status) === 'online' ? Colors.GREEN2 : (hub.status === 'offline') ? Colors.RED2 : Colors.ORANGE2, paddingRight: "5px"}}></span> 
+                                        {hub.name} <small className={Classes.TEXT_MUTED}>{hub.serialNumber}</small>
+                                    </h5>
+                                    <div className="row">
+                                        <div className="col-xs">
+                                            <div className="box">
+                                                <small className="pt-text-muted">Status</small><br />
+                                                <Tag className={Classes.MINIMAL} intent={this.statusIntent(hub.status)}> <span className="pt-icon-pulse"></span> {hub.status}</Tag>
+                                            </div>
+                                        </div>
+                                        <div className="col-xs">
+                                            <div className="box">
+                                                <small className="pt-text-muted">Sensors</small><br />
+                                                <Tag className={Classes.MINIMAL} intent={Intent.NONE}><span className="pt-icon-graph"></span> {hub.sensors.length}</Tag>
+                                            </div>
+                                        </div>
+                                        <div className="col-xs">
+                                            <div className="box">
+                                                <small className="pt-text-muted">Location</small><br />
+                                                {hub.room.name 
+                                                    ? ( <Tag className={Classes.MINIMAL} intent={Intent.PRIMARY}><span className="pt-icon-geolocation"></span> {hub.room.name}</Tag> ) 
+                                                    : ( "-" )
+                                                }
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         ))
@@ -62,6 +86,10 @@ class Hubs extends Component {
                 </div>
             </div>
         )
+    }
+
+    statusIntent(status) {
+        return (status) === 'online' ? Intent.SUCCESS : (status === 'offline') ? Intent.DANGER : Intent.WARNING
     }
 }
 
