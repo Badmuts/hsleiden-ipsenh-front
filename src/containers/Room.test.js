11d9cfc3c55f7956e4858678da715f0cc558befd
Brandon0172 <brandonvanwijk@gmail.com>
2017-06-04 14:33:55 +0200

    fix test for Room.js

--- src/containers/Room.test.js
+++ src/containers/Room.test.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Room from './Room';
 import {MemoryRouter} from 'react-router-dom'
+import {Route} from 'react-router-dom'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  const params = {
+      buildingId: "",
+      roomId: ""
+  };
   ReactDOM.render(
     <MemoryRouter>
-      <Room />
-    </MemoryRouter>, div);
+      <Route render={(params) => (    
+          <Room {...params}/>
+      )}/>
+    </MemoryRouter>, div )
 });
