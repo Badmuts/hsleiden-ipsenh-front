e7479d3daf5da0af49d5ecf803d9e8a37f61e33b
Daan Rosbergen <d.rosbergen@gmail.com>
2017-11-29 09:12:46 +0100

    Remove settings cog in navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,5 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import {
-  Button,
-  Menu,
-  MenuItem,
-  MenuDivider,
-  Popover,
-  Position
-} from "@blueprintjs/core";
-
-const menu = (
-  <Menu>
-    <MenuItem text="New" />
-    <MenuItem text="Open" />
-    <MenuItem text="Save" />
-    <MenuDivider />
-    <MenuItem text="Settings..." />
-  </Menu>
-);
 
 class Nav extends Component {
   constructor(props) {
@@ -38,9 +20,6 @@ class Nav extends Component {
             Buildings
           </Link>
           <Link className="pt-button pt-minimal" to="/hubs">Hubs</Link>
-          <Popover content={menu} position={Position.BOTTOM_RIGHT}>
-            <Button className="pt-minimal" iconName="cog" />
-          </Popover>
         </div>
       </nav>
     );
